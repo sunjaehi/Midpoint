b0728b2d4914749bb78d59cad63e77b145ef71fa
Lee Sunjae <sunjae7944@naver.com>
2024-10-10 23:38:55 +0900

    feat : form 형식, 버튼 추가

--- capstone/src/page/Mainpage.jsx
+++ capstone/src/page/Mainpage.jsx
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+
+const Custombutton = styled(Button)`
+    border-radius : 10px;
+    border : none;
+    margin-top : 40px;
+    background-Color : #FFD09B;
+    display : flex;
+    position:absolute;
+    justify-content : space-between;
+    color:black;
+    box-shadow :0 4px 8px rgba(0,0,0,0.2);
+    
+    &:hover {
+        background-color: #FFD060;
+        color :black;
+    }
+`;
 
 function MainPage() {
     return (
-        <div>Mainpage</div>
+        <div style={{background : '#FFF7D1', height:'100vh'}}>
+        <Container>
+            <Col>
+            <Row>
+                <div style={{textAlign : 'center', marginTop : '50px', fontSize : '30px', fontWeight:'lighter'}}>원하는 장소를 입력해주세요</div>
+                <Form>
+                    <Form.Group className='place-1' controlId='place01'>
+                        <Form.Control type='text' placeholder='장소1을 입력해주세요' 
+                            style={{borderRadius : '15px', border : 'none', backgroundColor : '#FFECC8', marginTop : '60px', height : '40px'}}/>
+                    </Form.Group>
+                </Form>
+                <Form>
+                    <Form.Group className='place-2' controlId='place02'>
+                        <Form.Control type='text' placeholder='장소2를 입력해주세요' 
+                            style={{borderRadius : '15px', border : 'none', backgroundColor : '#FFECC8', marginTop : '60px', height : '40px'}}/>
+                    </Form.Group>
+                </Form>
+            </Row>
+            <Row>
+                <Custombutton type='button' value="input" style={{width:'40%'}}>
+                    장소 추가하기
+                </Custombutton>
+                <Custombutton type='button' value="submit" style={{width:'40%', right:0}}>
+                    장소 검색하기
+                </Custombutton>
+            </Row>
+            </Col>
+        </Container>
+        </div>   
     );
 }
 
