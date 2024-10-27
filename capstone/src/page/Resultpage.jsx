21ec3ae131ec08661c2916a852c7d92bd2e33eed
Lee Sunjae <sunjae7944@naver.com>
2024-10-27 21:23:06 +0900

    feat : Kakao map 마커 생성 구현

--- capstone/src/page/Resultpage.jsx
+++ capstone/src/page/Resultpage.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, {useState, useEffect, useRef } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function Resultpage() {
+    const [state, setState] = useState({
+        center:{lat: 37.6159738, lng: 127.0115837},
+        
+    });
+    const [level, setLevel] = useState(5);
+    const mapRef = useRef();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const map = mapRef.current;
+        const currentLat = map.getCenter().getLat();
+        const currentLng = map.getCenter().getLng();
+
+    }
+
     return (
-        <div>Resultpage</div>
+        <div>
+            <div style={{position:'relative', width:'100vw', height:'100vh'}}>
+                <Map    
+                    center={state.center}
+                    style={{width:"100%", height:"100%"}}
+                    level={level}
+                    ref={mapRef}
+                >
+                    <MapMarker
+                        position={state.center}
+                        image={{
+                            src:"https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+                            size:{width: 30, height:45},
+                        }}
+                    />
+                </Map>
+            </div>
+
+        </div>
     );
 }
 
