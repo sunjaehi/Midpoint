ee6cf4bb1a5be5e6b77fc3c45a1a7b00e5f39ca4
Lee Sunjae <sunjae7944@naver.com>
2024-10-29 21:49:28 +0900

    design : router button 추가

--- capstone/src/page/Inputpage.jsx
+++ capstone/src/page/Inputpage.jsx
@@ -1,10 +1,38 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+// import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import { useNavigate } from 'react-router-dom';
+
+const Custombutton = styled.button`
+    border-radius : 10px;
+    border : none;
+    background-color : #FFD09B;
+    color: black;
+    box-shadow : 0 4px 8px rgba(0,0,0,0.2);
+    
+    &:hover {
+        background-color: #FFD060;
+        color : black;
+    }
+    &:active {
+        background-color: #FFD060;
+        color : black;
+    }
+`;
 
 const AutoCompleteSearch = () => {
-    const [query, setQuery] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
+    const navigate = useNavigate();
+    const navigateToResult = () => {
+        navigate("/Resultpage");
+    }
 
-    const handleInputChange = (e) => {
+    const [query1, setQuery1] = useState('');
+    const [query2, setQuery2] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+    const [activeInput, setActiveInput] = useState(null);
+
+    const handleInputChange = (e, setQuery) => {
         const input = e.target.value;
         setQuery(input);
 
@@ -12,7 +40,7 @@ const AutoCompleteSearch = () => {
             const ps = new window.kakao.maps.services.Places();
             ps.keywordSearch(input, (data, status) => {
                 if (status === window.kakao.maps.services.Status.OK) {
-                    setSuggestions(data); // 검색 결과 설정
+                    setSuggestions(data);
                 } else {
                     setSuggestions([]);
                 }
@@ -22,33 +50,114 @@ const AutoCompleteSearch = () => {
         }
     };
 
-    const handleSuggestionClick = (suggestion) => {
+    const handleSuggestionClick = (suggestion, setQuery) => {
         setQuery(suggestion.place_name);
         setSuggestions([]);
     };
 
     return (
-        <div style={{ width: '300px', margin: '0 auto', position: 'relative' }}>
-            <input
-                type="text"
-                placeholder="검색어를 입력하세요"
-                value={query}
-                onChange={handleInputChange}
-                style={{ padding: '10px', fontSize: '16px', width: '100%', borderRadius: '4px', border: '1px solid #ddd' }}
-            />
-            {suggestions.length > 0 && (
-                <ul style={{ listStyleType: 'none', padding: 0, marginTop: '5px', border: '1px solid #ddd', borderRadius: '4px', background: 'white', maxHeight: '200px', overflowY: 'auto', position: 'absolute', width: '100%', zIndex: 10 }}>
-                    {suggestions.map((suggestion) => (
-                        <li
-                            key={suggestion.id}
-                            onClick={() => handleSuggestionClick(suggestion)}
-                            style={{ padding: '10px', cursor: 'pointer' }}
-                        >
-                            {suggestion.place_name}
-                        </li>
-                    ))}
-                </ul>
-            )}
+        <div style={{background : '#FFF7D1', height:'100vh'}}>
+            <div style={{textAlign : 'center', fontSize : '30px', fontWeight:'lighter', paddingTop:'60px'}}>
+                원하는 장소를 입력해주세요
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '100px', paddingTop : '50px'}}>
+                {/* 검색창 1 */}
+                <div style={{ width: '400px', position: 'relative' }}>
+                    <input
+                        type="text"
+                        placeholder="원하는 장소를 입력해주세요"
+                        value={query1}
+                        onChange={(e) => handleInputChange(e, setQuery1)}
+                        onFocus={()=>setActiveInput(1)}
+                        style={{
+                            padding: '10px',
+                            fontSize: '16px',
+                            width: '100%',
+                            borderRadius: '10px',
+                            border: '1px solid #ddd',
+                            height: '50px'
+                        }}
+                    />
+                    {activeInput === 1 && suggestions.length > 0 && (
+                        <ul style={{
+                            listStyleType: 'none',
+                            padding: 0,
+                            marginTop: '5px',
+                            border: '1px solid #ddd',
+                            borderRadius: '4px',
+                            background: 'white',
+                            maxHeight: '200px',
+                            overflowY: 'auto',
+                            position: 'absolute',
+                            width: '100%',
+                            zIndex: 10
+                        }}>
+                            {suggestions.map((suggestion) => (
+                                <li
+                                    key={suggestion.id}
+                                    onClick={() => handleSuggestionClick(suggestion, setQuery1)}
+                                    style={{ padding: '10px', cursor: 'pointer' }}
+                                >
+                                    {suggestion.place_name}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+
+                {/* 검색창 2 */}
+                <div style={{ width: '400px', position: 'relative' }}>
+                    <input
+                        type="text"
+                        placeholder="원하는 장소를 입력해주세요"
+                        value={query2}
+                        onChange={(e) => handleInputChange(e, setQuery2)}
+                        onFocus={()=>setActiveInput(2)}
+                        style={{
+                            padding: '10px',
+                            fontSize: '16px',
+                            width: '100%',
+                            borderRadius: '10px',
+                            border: '1px solid #ddd',
+                            height: '50px'
+                        }}
+                    />
+                    {activeInput === 2 && suggestions.length > 0 && (
+                        <ul style={{
+                            listStyleType: 'none',
+                            padding: 0,
+                            marginTop: '5px',
+                            border: '1px solid #ddd',
+                            borderRadius: '4px',
+                            background: 'white',
+                            maxHeight: '200px',
+                            overflowY: 'auto',
+                            position: 'absolute',
+                            width: '100%',
+                            zIndex: 10
+                        }}>
+                            {suggestions.map((suggestion) => (
+                                <li
+                                    key={suggestion.id}
+                                    onClick={() => handleSuggestionClick(suggestion, setQuery2)}
+                                    style={{ padding: '10px', cursor: 'pointer' }}
+                                >
+                                    {suggestion.place_name}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+            </div>
+            <Row style={{marginTop:'80px', display : 'flex', justifyContent : 'center', gap : '300px'}}>
+                <Custombutton type='button' value="input" style={{width:'200px', height:'40px', padding:'0'}}>
+                    장소 추가하기
+                </Custombutton>
+                <Custombutton type='button' value="submit" style={{width:'200px', height:'40px', padding:'0'}} onClick={navigateToResult}>
+                    장소 검색하기
+                </Custombutton>
+            </Row>
         </div>
     );
 };
