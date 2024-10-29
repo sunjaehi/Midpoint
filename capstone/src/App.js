87afe3eba2e11094a86c172db9aab7b3624c4564
Lee Sunjae <sunjae7944@naver.com>
2024-10-29 18:36:48 +0900

    feat : router 추가

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainPage from './page/Mainpage';
+import Inputpage from './page/Inputpage';
+import Resultpage from './page/Resultpage';
 
 function App() {
   return (
-    <div>
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Inputpage />}></Route>
+        <Route path='/Resultpage' element={<Resultpage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
