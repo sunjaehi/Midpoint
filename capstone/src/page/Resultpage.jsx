7059dd1ad41b83ad131ad1da22a6fa2d20a334bb
Lee Sunjae <sunjae7944@naver.com>
2024-11-11 17:16:15 +0900

    feat : 3개의 MapMarker가 한페이지에 보이도록 수정

--- capstone/src/page/Resultpage.jsx
+++ capstone/src/page/Resultpage.jsx
@@ -1,39 +1,46 @@
-import React, { useEffect, useRef } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const Resultpage = () => {
     const location = useLocation();
-    const navigate = useNavigate();
     const mapRef = useRef();
     const { midpoint, location1, location2 } = location.state || {};
 
-    useEffect(() => {
+    const handleMapLoad = (map) => {
+        mapRef.current = map;
 
-        if (mapRef.current && location1 && location2 && midpoint) {
-            const map = mapRef.current;
+        if (location1 && location2 && midpoint) {
             const bounds = new window.kakao.maps.LatLngBounds();
 
             bounds.extend(new window.kakao.maps.LatLng(location1.latitude, location1.longitude));
             bounds.extend(new window.kakao.maps.LatLng(location2.latitude, location2.longitude));
             bounds.extend(new window.kakao.maps.LatLng(midpoint.latitude, midpoint.longitude));
 
-            map.setBounds(bounds);
+            map.setBounds(bounds); // 모든 위치가 보이도록 범위 설정
         }
-    }, [midpoint, location1, location2]);
+    };
 
     return (
-        <Map center={{ lat: midpoint.latitude, lng: midpoint.longitude }} level={3} style={{ width: "100%", height: "100vh" }} onCreate={(map)=>(mapRef.current = map)}>
+        <Map
+            center={{ lat: midpoint.latitude, lng: midpoint.longitude }}
+            style={{ width: "100%", height: "100vh" }}
+            onCreate={handleMapLoad} // 지도가 생성될 때 handleMapLoad 호출
+        >
             {/* 장소 1 마커 */}
             {location1 && (
                 <MapMarker position={{ lat: location1.latitude, lng: location1.longitude }}>
-                    <div style={{ color: "#000", borderRadius : "5px", background : "white", padding : "5px" }}>장소 1 : {location1.name}</div>
+                    <div style={{ color: "#000", borderRadius: "5px", background: "white", padding: "5px" }}>
+                        장소 1 : {location1.name}
+                    </div>
                 </MapMarker>
             )}
             {/* 장소 2 마커 */}
             {location2 && (
                 <MapMarker position={{ lat: location2.latitude, lng: location2.longitude }}>
-                    <div style={{ color: "#000", borderRadius : "5px", background : "white", padding : "5px" }}>장소 2 : {location2.name}</div>
+                    <div style={{ color: "#000", borderRadius: "5px", background: "white", padding: "5px" }}>
+                        장소 2 : {location2.name}
+                    </div>
                 </MapMarker>
             )}
             {/* 중간지점 마커 */}
